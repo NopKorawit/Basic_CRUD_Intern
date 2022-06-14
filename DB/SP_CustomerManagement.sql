afa2942c28483eb7439cb3a4c4786e9e4160e53f
NopKorawit <korawit.was@gmail.com>
2022-06-14 16:45:00 +0700

    Init

---
+++ DB/SP_CustomerManagement.sql
@@ -0,0 +1,67 @@
+USE [Homework2]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[SP_CustomerManagement]
+	@Process NVARCHAR(30) = NULL,
+	@Id INT = -1,
+	@FirstName NVARCHAR(80) = NULL,
+	@LastName NVARCHAR(80) = NULL,
+	@Address NVARCHAR(160) = NULL,
+	@Birthday INT = NULL
+
+AS
+BEGIN
+	IF @Process = 'CREATE' 
+		BEGIN
+			INSERT INTO TB_Customer(FirstName,LastName, Address, Birthday)
+			VALUES (@FirstName,@LastName, @Address, @Birthday)
+		END
+
+	ELSE IF @Process = 'UPDATE'
+		BEGIN
+			UPDATE TB_Customer
+			SET FirstName = @FirstName,
+				LastName = @LastName,
+				Address = @Address,
+				Birthday = @Birthday
+			WHERE Id = @Id
+		END
+
+	ELSE IF @Process = 'DELETE'
+		BEGIN
+			UPDATE TB_Customer
+			SET Delflag = 1
+			WHERE Id = @Id
+		END
+
+	ELSE IF @Process = 'FIND'
+		BEGIN
+			SELECT  c.Id,
+					c.FirstName,
+					c.LastName,
+					c.Address,
+					c.Birthday
+			FROM TB_Customer c
+			WHERE Id = @Id
+			AND Delflag = 0
+		END
+	ELSE 
+		BEGIN
+			SELECT  c.Id,
+					c.FirstName,
+					c.LastName,
+					c.Address,
+					c.Birthday
+			FROM TB_Customer c
+			WHERE Delflag = 0
+		END
+END
+GO
+
+
---
+++ DB/TB_Customer.sql
@@ -0,0 +1,27 @@
+USE [Homework2]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TB_Customer](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](80) NULL,
+	[LastName] [nvarchar](80) NULL,
+	[Address] [nvarchar](160) NULL,
+	[Birthday] [int] NULL,
+	[Delflag] [int] NULL,
+ CONSTRAINT [Id] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[TB_Customer] ADD  CONSTRAINT [DF_TB_Customer_Delflag]  DEFAULT ((0)) FOR [Delflag]
+GO
+
+
